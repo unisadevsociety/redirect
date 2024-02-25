31a1973ad5976277b94448147d595cda4596b730
Elton-Maruthulle <eltonmaruthulle@gmail.com>
2024-02-25 19:35:17 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Link } from "react-router-dom";
+import Lottie from "lottie-react";
+import animationData2 from "./animation/anim (2).json";
+import animationData1 from "./animation/anim (1).json";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="congratswrapper">
+        <div className="congratsAnimation3">
+          <Lottie animationData={animationData2} />
+        </div>
+        <div className="congratsAnimation3">
+          <Lottie animationData={animationData1} />
+        </div>
+        <div className="congratstext">
+          <h2>Congratulations!</h2>
+          <p>Welcome on board! Thank you for joining us.</p>
+          {/* Add a Link to the home page */}
+          <div className="goToHemoButtonBorders">
+            <a
+              href="https://unisadevsociety.github.io/dev/"
+              className="goToHemoButton"
+            >
+              Go to Home
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
